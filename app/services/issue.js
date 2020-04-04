160c1ac6017a7dbb852d5d72e09125fe07b6ee47
Marcus Spiegel <spiegel@uscreen.de>
2020-04-04 16:04:09 +0200

    bumb minimist in deps to ^0.2.1

--- app/services/issue.js
+++ app/services/issue.js
@@ -4,8 +4,8 @@ module.exports = async fastify => {
   const collection = fastify.mongo.db.collection('issue')
 
   // breaks
-  collection.createIndex({ id: 1 }, { unique: true })
+  // collection.createIndex({ id: 1 }, { unique: true })
 
   // fixes
-  // await collection.createIndex({ id: 1 }, { unique: true })
+  await collection.createIndex({ id: 1 }, { unique: true })
 }
